f25997c5aa21414ac2170bb38c977535831dd3c9
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-02 16:09:08 +0300

    Brew formula update for oictl version 0.13.1

--- Formula/oictl.rb
+++ Formula/oictl.rb
@@ -2,16 +2,16 @@
 class Oictl < Formula
   desc ""
   homepage ""
-  version "0.13.0"
+  version "0.13.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/open-integration/oictl/releases/download/0.13.0/oictl_0.13.0_Darwin_x86_64.tar.gz"
-    sha256 "b8107bbe929c8e14457ec83b21410ee30dd77e17601e8c2965b355b84956a604"
+    url "https://github.com/open-integration/oictl/releases/download/0.13.1/oictl_0.13.1_Darwin_x86_64.tar.gz"
+    sha256 "ef7cf30b1721c28bff082966a54af10a0a5cce69d9e03c71a6322e0e4344e57a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/open-integration/oictl/releases/download/0.13.0/oictl_0.13.0_Linux_x86_64.tar.gz"
-      sha256 "4c169be444d90f54689dfb9f746e2ff292e74be21eade7f78f37607fcfc81733"
+      url "https://github.com/open-integration/oictl/releases/download/0.13.1/oictl_0.13.1_Linux_x86_64.tar.gz"
+      sha256 "cbfb8aa74c5b4684db5a6f0faee1d96d3b84d5142d830cc2de7ee20f1780400e"
     end
   end
   
